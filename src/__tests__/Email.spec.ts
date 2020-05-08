e179b2f41aafedc00e22cd306f17dfe3e8b6f438
brunocheble <contato.cheble@gmail.com>
2020-05-08 15:52:59 +0100

    Criação do test automático para envio de e-mail.

--- src/__tests__/Email.spec.ts
+++ src/__tests__/Email.spec.ts
@@ -2,12 +2,19 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Email', () => {
-  it('should be able to execute a email route', async () => {
-    const response = await request(app).get('/emails');
+  it('should be able to execute a sending email route', async () => {
+    const response = await request(app)
+      .post('/emails')
+      .send({
+        emails: ['contato.cheble@gmail.com'],
+        subject: 'Test Sending email',
+        body: '<h1>Great! The test passed</h1>',
+        category: 'Eletronics',
+      });
 
     expect(response.body).toEqual(
       expect.objectContaining({
-        message: 'estou aqui',
+        sent_emails: ['contato.cheble@gmail.com'],
       }),
     );
   });
